5737e955d753007c5080a7fe6e9d6637e31d0d2b
Md. Siam Biswas <siam.biswas@icloud.com>
2022-11-26 16:26:04 -0500

    Config Changes

--- api-gatway/src/main/java/com/miu/APIGatewayApplication.java
+++ api-gatway/src/main/java/com/miu/APIGatewayApplication.java
@@ -2,15 +2,11 @@ package com.miu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.reactive.ReactiveUserDetailsServiceAutoConfiguration;
 import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
 import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
 import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
-import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 @EnableEurekaClient
--- rating-service/src/main/java/com/miu/ratingservice/controller/RatingController.java
+++ rating-service/src/main/java/com/miu/ratingservice/controller/RatingController.java
@@ -7,10 +7,7 @@ import com.miu.ratingservice.entity.ContentType;
 import com.miu.ratingservice.entity.Ratings;
 import com.miu.ratingservice.service.RatingService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
